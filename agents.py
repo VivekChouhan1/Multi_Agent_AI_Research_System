984c91165e176dab03e166d3b314b0074f7295c0
VivekChouhan1 <vivekchouhan2512@gmail.com>
2026-06-07 16:38:38 +0530

    initial commit

---
+++ agents.py
@@ -0,0 +1,25 @@
+from langchain.agents import create_agent
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from tools import web_search, scrape_url_bs
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+# Model Setup
+llm_gemini = ChatGoogleGenerativeAI(model="gemini-2.5-flash-lite", temperature=0)
+
+# 1-st agent
+def build_search_agent():
+    return create_agent(
+        model=llm_gemini,
+        tools=[web_search]
+    )
+
+# 2-nd agent
+def build_reader_agent():
+    return create_agent(
+        model=llm_gemini,
+        tools=[scrape_url_bs]
+    )
---
+++ tools.py
@@ -0,0 +1,38 @@
+from langchain.tools import tool
+import requests
+from bs4 import BeautifulSoup
+from tavily import TavilyClient
+import os
+from dotenv import load_dotenv
+load_dotenv()
+from rich import print
+
+tavily=TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
+
+
+@tool
+def web_search(query:str):
+    """Search the web for recent and reliable information on a topic. Return Titles, URLs and snippets"""
+    results=tavily.search(query=query,max_results=5)
+
+    web_output=[]
+
+    for res in results['results']:
+        web_output.append(
+            f"Title: {res['title']}\nURL: {res['url']}\nSnippet: {res['content'][:300]}\n" 
+        )
+    return "\n----\n".join(web_output)
+
+
+@tool
+def scrape_url_bs(url : str) -> str:
+    """ Scrape and return clean text content from a given URL for deeper reading."""
+    try:
+        res = requests.get(url, timeout=8, headers={"User-Agent": "Mozilla/5.0"})
+        soup = BeautifulSoup(res.text, "html.parser")
+        for tag in soup(["script", "style", "nav", "footer"]):
+            tag.decompose()
+        return soup.get_text(separator=" ", strip=True)[:3000]
+    except Exception as e:
+        return f"Could not scrape URL: {str(e)}"
+    
